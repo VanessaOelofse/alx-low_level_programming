d8cbd7439a00e6a2db50b57a2068a8f1271e51df
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-11 23:38:13 +0200

    Task 12 fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,8 @@ putchar(num1 % 10 + '0');
 putchar(' ');
 putchar(num2 / 10 + '0');
 putchar(num2 % 10 + '0');
-if (!(num1 == 98 && num2 == 99)){
+if (!(num1 == 98 && num2 == 99))
+{
 putchar(',');
 putchar(' ');
 }
