ee83d9731242ae53133fc59239ed5658ad48c5fb
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-11 21:09:46 +0200

    Task 9 All single digit combinations ascending.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - Prints all single digits with , and space followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+int digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+if (digit == '9')
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
