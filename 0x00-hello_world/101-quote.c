35741f537be2287d4c9addeee939694013c8351d
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-12 20:50:09 +0200

    Apply fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - main block
- * Return: 1
- */
+* main - main function in the task
+*
+*
+*Return: return 1
+*/
 int main(void)
 {
-int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-write(2,
-"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-s);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
