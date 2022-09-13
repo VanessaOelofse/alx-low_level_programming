aecb24255fc794679db401b70d715e9487c5d8c2
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-13 20:35:15 +0200

    Task 2, Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Print alphabet 10 x
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+print_alphabet();
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,5 @@
  *Return: Always 0
  */
 
-void print_alphabet(void);
+void print_alphabet();
+void print_alphabet_x10(void);
