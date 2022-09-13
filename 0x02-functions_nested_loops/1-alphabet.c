6109c5775351a5935732e63f5ae2869df5395227
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-13 18:55:09 +0200

    Fixed lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * Main- Prints alphabet Lowercase
  * 
- * Return - 0
+ * 
  */
 
 void print_alphabet(void)
@@ -14,5 +14,4 @@ for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 }
 _putchar('\n');
-return (0);
 }
