669770b095e6d7808a2836b551c9eaa229637a40
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-13 22:00:47 +0200

    Task 6, computes value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value of integer
+ * @n: Integer
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+}
