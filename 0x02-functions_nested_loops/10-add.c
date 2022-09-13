9610b22b47500143139b5a5d0bda6569cb61c96d
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-13 22:30:16 +0200

    Task 10 add A and B

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two ints and returns result
+ * @a: Get first int
+ * @b: Get second int
+ * Return: Result of two inputs
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
