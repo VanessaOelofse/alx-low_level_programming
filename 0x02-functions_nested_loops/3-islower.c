4f732e1f9a3d275c6eceaeabbc36f9f893f30b45
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-13 21:05:42 +0200

    Task 3 print 1 for lower case c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _islower(int c) - Checks lower case
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
