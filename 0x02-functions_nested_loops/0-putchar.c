2395f6d51063a8d1b063f9d4f994569509911bb2
VanessaOelofse <Vanessa.fritz09@gmail.com>
2022-09-13 18:36:44 +0200

    Fixes for task _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include main.h
+#include "main.h"
 
 /**
  *main - Writes _putchar with new line
@@ -7,7 +7,7 @@
  *Return: Always 0
  */
 
-int _putchar(char *S)
+int _putchar(char *s)
 {
 char *s="_putchar\n";
 while (*s)
